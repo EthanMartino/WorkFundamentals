448d6c7d591181a4ea1f1667d14e86ca5915a823
EthanMartino <martino9448@student.cptc.edu>
2020-07-29 14:56:09 -0700

    Created Schedule class and an enum for days of the week

---
+++ WorkFundamentals/Models/Schedule.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorkFundamentals.Models
+{
+    public class Schedule
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public Employee Employee { get; set; }
+
+        public Boolean IsCurrent { get; set; }
+
+        public DateTime StartingDate { get; set; }
+
+        public Dictionary<Day, DateTime> DailySchedule { get; set; }
+
+        public Schedule()
+        {
+            DailySchedule = new Dictionary<Day, DateTime>()
+            {
+                { Day.Monday, new DateTime() },
+                { Day.Tuesday, new DateTime() },
+                { Day.Wednesday, new DateTime() },
+                { Day.Thursday, new DateTime() },
+                { Day.Firday, new DateTime() },
+                { Day.Saturday, new DateTime() },
+                { Day.Sunday, new DateTime() }
+            };
+        }
+    }
+
+    public enum Day
+    {
+        Monday, Tuesday, Wednesday, Thursday, Firday, Saturday, Sunday
+    }
+}
