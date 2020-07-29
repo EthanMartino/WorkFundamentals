f80ee25089a53d5a22c406716383329ddcb14be8
EthanMartino <martino9448@student.cptc.edu>
2020-07-29 14:57:05 -0700

    Created classes for Employees and Managers

---
+++ WorkFundamentals/Models/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorkFundamentals.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string JobTitle { get; set; }
+
+        public Schedule CurrentSchedule { get; set; }
+
+        public List<WorkTask> CurrentTasks { get; set; }
+
+        public List<WorkTask> CompletedTasks { get; set; }
+    }
+}
---
+++ WorkFundamentals/Models/Manager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorkFundamentals.Models
+{
+    public class Manager
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string JobTitle { get; set; }
+    }
+}
