d5d9dfcc58186ec92258c25be0e9bee479df687c
EthanMartino <martino9448@student.cptc.edu>
2020-07-29 14:56:41 -0700

    Created WorkTask class and associating WorkTaskCategory class

---
+++ WorkFundamentals/Models/WorkTask.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorkFundamentals.Models
+{
+    public class WorkTask
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public Employee Employee { get; set; }
+
+        public Schedule Schedule { get; set; }
+
+        public WorkTaskCategory Category { get; set; }
+    }
+
+    public class WorkTaskCategory
+    {
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+    }
+}
