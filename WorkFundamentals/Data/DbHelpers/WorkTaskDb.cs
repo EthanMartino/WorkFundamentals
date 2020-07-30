4212e6b466194abf463c1bf21d403942c2e7be7b
EthanMartino <martino9448@student.cptc.edu>
2020-07-30 14:02:57 -0700

    Full CRUD functionality for WorkTask

---
+++ WorkFundamentals/Data/DbHelpers/WorkTaskDb.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WorkFundamentals.Models;
+
+namespace WorkFundamentals.Data.DbHelpers
+{
+    public class WorkTaskDb
+    {
+        public async Task<List<WorkTask>> GetAllWorkTasks(ApplicationDbContext context)
+        {
+            List<WorkTask> workTasks = await context.WorkTasks
+                .OrderBy(wt => wt.Title)
+                .ToListAsync();
+
+            return workTasks;
+        }
+
+        public async Task<WorkTask> GetWorkTaskById(int id, ApplicationDbContext context)
+        {
+            WorkTask workTasks = await context.WorkTasks
+                .Where(wt => wt.WorkTaskId == id)
+                .SingleOrDefaultAsync();
+
+            return workTasks;
+        }
+
+        public async void Add(WorkTask workTask, ApplicationDbContext context)
+        {
+            await context.WorkTasks.AddAsync(workTask);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task<WorkTask> Update(WorkTask workTask, ApplicationDbContext context)
+        {
+            context.WorkTasks.Attach(workTask);
+            context.Entry(workTask).State = EntityState.Modified;
+            await context.SaveChangesAsync();
+
+            return workTask;
+        }
+
+        public async void Delete(WorkTask workTask, ApplicationDbContext context)
+        {
+            context.WorkTasks.Attach(workTask);
+            context.Entry(workTask).State = EntityState.Deleted;
+            await context.SaveChangesAsync();
+        }
+    }
+}
