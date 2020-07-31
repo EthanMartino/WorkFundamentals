384b872d40a5f363c73950fcbf736975616a57a1
EthanMartino <martino9448@student.cptc.edu>
2020-07-30 20:16:06 -0700

    Created a field for the ApplicationDbContext

--- WorkFundamentals/Controllers/ScheduleController.cs
+++ WorkFundamentals/Controllers/ScheduleController.cs
@@ -2,12 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using WorkFundamentals.Data;
 
 namespace WorkFundamentals.Controllers
 {
     public class ScheduleController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public ScheduleController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
