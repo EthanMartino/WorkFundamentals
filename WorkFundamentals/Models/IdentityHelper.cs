d1c886ce0e362c512da3dcf33b04932604156eaf
EthanMartino <martino9448@student.cptc.edu>
2020-08-27 13:10:14 -0700

    Set Sign in and password options for Identity Users

---
+++ WorkFundamentals/Models/IdentityHelper.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorkFundamentals.Models
+{
+    public class IdentityHelper
+    {
+        public static void SetIdentityOptions(IdentityOptions options)
+        {
+            options.SignIn.RequireConfirmedEmail = true;
+            options.SignIn.RequireConfirmedPhoneNumber = false;
+
+            options.Password.RequireDigit = true;
+            options.Password.RequiredLength = 6;
+            options.Password.RequireLowercase = false;
+            options.Password.RequireUppercase = true;
+
+            options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(30);
+            options.Lockout.MaxFailedAccessAttempts = 5;
+        }
+    }
+}
