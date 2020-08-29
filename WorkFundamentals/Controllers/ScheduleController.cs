f903b27a29e47e2d04ad1c214b66eb72a75ca5e6
EthanMartino <martino9448@student.cptc.edu>
2020-08-28 21:54:11 -0500

    Added validation for employer role

--- WorkFundamentals/Controllers/ScheduleController.cs
+++ WorkFundamentals/Controllers/ScheduleController.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using WorkFundamentals.Data;
@@ -13,10 +15,12 @@ namespace WorkFundamentals.Controllers
     public class ScheduleController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
 
-        public ScheduleController(ApplicationDbContext context)
+        public ScheduleController(ApplicationDbContext context, UserManager<IdentityUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
         public IActionResult Index()
@@ -27,6 +31,16 @@ namespace WorkFundamentals.Controllers
         public async Task<IActionResult> ViewAllSchedules()
         {
             List<Schedule> schedules = await ScheduleDb.GetAllSchedules(_context);
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            if (userId != null && userId != String.Empty)
+            {
+                IdentityUser currUser = await _userManager.FindByIdAsync(userId);
+                await _userManager.AddToRoleAsync(currUser, IdentityHelper.Employer); //Hard coded Employer for testing
+                if (await _userManager.IsInRoleAsync(currUser, IdentityHelper.Employer))
+                {
+                    ViewBag.IsEmployer = true;
+                }
+            }
 
             return View(schedules);
         }
@@ -37,6 +51,7 @@ namespace WorkFundamentals.Controllers
             return View();
         }
 
+        [Authorize(Roles = IdentityHelper.Employer)]
         [HttpPost]
         public async Task<IActionResult> CreateSchedule(int id)
         {
